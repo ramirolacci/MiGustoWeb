71f19a8a63386f19e3a8b887bfcd2263b056b97c
Joaquin Tonizzo <joaquintonizzo1@gmail.com>
2025-05-27 22:07:48 -0300

    fixes navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
       <div className="container-fluid">
-        <a className="navbar-brand d-flex align-items-center" href="#">
+        <a className="navbar-brand d-flex align-items-center" href="https://www.migusto.com.ar" target="_blank" rel="noopener noreferrer">
           <img
             src="https://www.migusto.com.ar/assets/images/logoMGBlanco.png"
             alt="Mi Gusto"
@@ -27,13 +27,13 @@ const Navbar: React.FC = () => {
         <div className="collapse navbar-collapse" id="navbarMenu">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link" href="#">Pedir</a>
+              <a className="nav-link" href="https://pedir.migusto.com.ar/" target="_blank" rel="noopener noreferrer">Pedir</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">Carta</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Sucursales</a>
+              <a className="nav-link" href="/sucursales">Sucursales</a>
             </li>
           </ul>
         </div>
