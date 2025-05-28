0d6319bfc5371b1dc70055b97fd53fbd0012fd87
Joaquin Tonizzo <joaquintonizzo1@gmail.com>
2025-05-28 14:40:30 -0300

    add home

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+// src/components/Home.jsx
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="home">
+            <h1>Bienvenido a Nuestra Tienda</h1>
+            <p>Explorá nuestros productos, descubrí la carta o encontrá tu sucursal más cercana.</p>
+        </div>
+    );
+}
+
+export default Home;
