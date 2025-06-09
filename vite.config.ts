159a85fd218932e81ac702e105ad9becdb6d9506
Facu Carrizo <facu14carrizo@gmail.com>
2025-06-09 11:56:58 -0300

    Carta improvement

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      '54e6-2803-9810-b097-f910-8d3d-cb35-65d8-6446.ngrok-free.app',
+      '.ngrok-free.app'
+    ]
+  }
 })
