3fc09b167225f68d9200cee9a9318f1503e6821e
Ramiro <ramiroalejandrolacci19@gmail.com>
2025-06-10 09:12:13 -0300

    fix things

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,168 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
-import { Link } from "react-router-dom";
 
-declare global {
-  interface Window {
-    fcn_goAPP: (type: number) => void;
-    fcn_open_contact: (type: string) => void;
-  }
-}
-
-const Footer = () => {
-  const [showScrollTop, setShowScrollTop] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      setShowScrollTop(scrollPosition > 300);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
-    // Aquí se agregará la redirección cuando se implemente
-    console.log('Link clicked:', e.currentTarget.href);
-  };
-
+const Footer: React.FC = () => {
   return (
-    <footer className="main-footer">
-      <div className="footer-container">
-        <div className="footer-row">
-          {/* Logo y marca */}
-          <div className="footer-col-md-3">
-            <a href="#top" className="footer-logo-link" onClick={handleLinkClick}>
-              <img
-                src="https://www.migusto.com.ar/assets/images/logoMGBlanco.png"
-                alt="Mi Gusto Empanadas de Verdad"
-                className="footer-logo-img"
-              />
-            </a>
-            <p className="footer-brand-text">
-              Experiencias de Verdad.
-            </p>
-          </div>
-
-          {/* Enlaces */}
-          <div className="footer-col-md-3">
-            <ul className="footer-links-list">
-              <li className="footer-link-item">
-                <Link to="/proveedores" className="footer-link">
-                  Proveedores
-                </Link>
-              </li>
-              <li className="footer-link-item">
-                <Link to="/trabaja-con-nosotros" className="footer-link">
-                  Trabajá con nosotros
-                </Link>
-              </li>
-              <li className="footer-link-item">
-                <Link to="/franquicias" className="footer-link">
-                  Franquicias
-                </Link>
-              </li>
-              <li className="footer-link-item">
-                <a href="#" className="footer-link">
-                  Legales
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          {/* Redes sociales y apps */}
-          <div className="footer-col-md-4">
-            <h6 className="footer-social-title">ENCONTRANOS EN</h6>
-            <div className="footer-social-icons">
-              <a href="#" className="footer-social-icon-link" onClick={handleLinkClick}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
-                </svg>
-              </a>
-              <a href="#" className="footer-social-icon-link" onClick={handleLinkClick}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                  <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
-                </svg>
-              </a>
-              <a href="#" className="footer-social-icon-link" onClick={handleLinkClick}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                  <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
-                </svg>
-              </a>
-              <a href="#" className="footer-social-icon-link" onClick={handleLinkClick}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                  <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
-                </svg>
-              </a>
-              <a href="#" className="footer-social-icon-link" onClick={handleLinkClick}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                  <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z" fill="#fff" />
-                </svg>
-              </a>
-            </div>
-            <div className="footer-app-links">
-              <a 
-                href="#"
-                className="footer-app-link"
-                onClick={handleLinkClick}
-              >
-                <img
-                  src="src/assets/google-play-logo.svg"
-                  alt="Google Play"
-                  width="120"
-                />
-              </a>
-              <a 
-                href="#"
-                className="footer-app-link"
-                onClick={handleLinkClick}
-              >
-                <img
-                  src="src/assets/app-store-apple-logo.svg"
-                  alt="App Store"
-                  width="120"
-                />
-              </a>
-            </div>
-          </div>
+    <footer className="footer-distributed">
+      <div className="footer-left">
+        <h3>Mi<span>Gusto</span></h3>
+        <p className="footer-links">
+          <Link to="/">Home</Link>
+          <Link to="/carta">Carta</Link>
+          <Link to="/productos">Productos</Link>
+          <Link to="/sucursales">Sucursales</Link>
+          <Link to="/nosotros">Nosotros</Link>
+          <Link to="/contacto">Contacto</Link>
+          <Link to="/franquicias">Franquicias</Link>
+          <Link to="/venta-corporativa">Venta Corporativa</Link>
+          <Link to="/proveedores">Proveedores</Link>
+          <Link to="/trabaja-con-nosotros">Trabajá con nosotros</Link>
+        </p>
+        <p className="footer-company-name">Mi Gusto © 2024</p>
+      </div>
+      <div className="footer-center">
+        <div>
+          <i className="fa fa-map-marker"></i>
+          <p><span>Av. Siempre Viva 742</span> Springfield, USA</p>
         </div>
-
-        <hr className="footer-divider" />
-
-        <div className="footer-row">
-          <div className="footer-col-12">
-            <p className="footer-final-text">
-              Desarrollado por el Departamento de Sistemas de <strong>Mi Gusto</strong>
-            </p>
-            <p className="footer-final-text">
-              <strong>Mi Gusto®</strong> es una marca de <strong>La Honoria Alimentos S.A.</strong> – Argentina CUIT: 30-71558654-8
-            </p>
-          </div>
+        <div>
+          <i className="fa fa-phone"></i>
+          <p>+1 555 123456</p>
         </div>
-
-        {/* Boton de scroll hacia arriba */}
-        {showScrollTop && (
-          <button
-            onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-            className="scroll-top-button"
-            aria-label="Volver arriba"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"
-              />
-            </svg>
-          </button>
-        )}
+        <div>
+          <i className="fa fa-envelope"></i>
+          <p><a href="mailto:support@migusto.com.ar">support@migusto.com.ar</a></p>
+        </div>
+      </div>
+      <div className="footer-right">
+        <p className="footer-company-about">
+          <span>Sobre la Compañía</span>
+          Mi Gusto es tu destino para el sabor y la calidad en cada bocado. Nos dedicamos a crear experiencias deliciosas y momentos inolvidables.
+        </p>
+        <div className="footer-icons">
+          <a href="#" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+          <a href="#" target="_blank" rel="noopener noreferrer"><i className="fab fa-tiktok"></i></a>
+          <a href="#" target="_blank" rel="noopener noreferrer"><i className="fab fa-youtube"></i></a>
+          <a href="#" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook"></i></a>
+          <a href="#" target="_blank" rel="noopener noreferrer"><i className="fab fa-x-twitter"></i></a>
+        </div>
       </div>
     </footer>
   );
--- src/pages/Proveedores.tsx
+++ src/pages/Proveedores.tsx
@@ -1,76 +1,108 @@
-import React from 'react';
-import './Contacto.css';
+import React, { useState } from 'react';
+import '../pages/Contacto.css';
 
-const Proveedores = () => {
+const Proveedores: React.FC = () => {
+  const [formData, setFormData] = useState({
+    nombreEmpresa: '',
+    razonSocial: '',
+    telefono: '',
+    email: '',
+    descripcion: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData(prevData => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Datos del formulario de proveedores:', formData);
+    // Aquí se manejaría el envío de datos, por ejemplo, a una API
+    alert('¡Gracias por tu interés! Nos pondremos en contacto pronto.');
+    setFormData({
+      nombreEmpresa: '',
+      razonSocial: '',
+      telefono: '',
+      email: '',
+      descripcion: '',
+    });
+  };
+
   return (
     <div className="sucursales-section">
       <div className="background-overlay"></div>
       <div className="sucursales-container">
         <div className="contacto-container">
           <div className="contacto-header">
-            <h1>Proveedores</h1>
-            <p>¿Querés ser proveedor de Mi Gusto? Completá el formulario y nos pondremos en contacto con vos.</p>
+            <h1>Formulario para Proveedores</h1>
+            <p>Completa el siguiente formulario si estás interesado en ser proveedor de Mi Gusto.</p>
           </div>
 
           <div className="contacto-content">
             <div className="contacto-form-container">
-              <h2>Envíanos tu Propuesta</h2>
-              <form className="contacto-form">
+              <h2>Información de Contacto</h2>
+              <form className="contacto-form" onSubmit={handleSubmit}>
                 <div className="form-row">
                   <div className="form-group half-width">
-                    <label htmlFor="nombre">Nombre de la empresa:<span className="required">*</span></label>
+                    <label htmlFor="nombreEmpresa">Nombre de la Empresa <span className="required">*</span></label>
                     <input
                       type="text"
-                      id="nombre"
-                      name="nombre"
-                      placeholder="Ingresá el nombre de tu empresa"
+                      id="nombreEmpresa"
+                      name="nombreEmpresa"
+                      value={formData.nombreEmpresa}
+                      onChange={handleChange}
                       required
                     />
                   </div>
                   <div className="form-group half-width">
-                    <label htmlFor="email">E-mail:<span className="required">*</span></label>
+                    <label htmlFor="email">E-mail <span className="required">*</span></label>
                     <input
                       type="email"
                       id="email"
                       name="email"
-                      placeholder="Ingresá tu e-mail"
+                      value={formData.email}
+                      onChange={handleChange}
                       required
                     />
                   </div>
                 </div>
                 <div className="form-row">
                   <div className="form-group half-width">
-                    <label htmlFor="telefono">Teléfono:<span className="required">*</span></label>
-                    <input
-                      type="tel"
-                      id="telefono"
-                      name="telefono"
-                      placeholder="Ingresá tu teléfono"
-                      required
-                    />
-                  </div>
-                  <div className="form-group half-width">
-                    <label htmlFor="razonSocial">Razón Social:<span className="required">*</span></label>
+                    <label htmlFor="razonSocial">Razón Social <span className="required">*</span></label>
                     <input
                       type="text"
                       id="razonSocial"
                       name="razonSocial"
-                      placeholder="Ingresá la razón social de tu empresa"
+                      value={formData.razonSocial}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div className="form-group half-width">
+                    <label htmlFor="telefono">Teléfono <span className="required">*</span></label>
+                    <input
+                      type="tel"
+                      id="telefono"
+                      name="telefono"
+                      value={formData.telefono}
+                      onChange={handleChange}
                       required
                     />
                   </div>
                 </div>
                 <div className="form-group">
-                  <label htmlFor="mensaje">Descripción de productos/servicios:</label>
+                  <label htmlFor="descripcion">Descripción de productos/servicios</label>
                   <textarea
-                    id="mensaje"
-                    name="mensaje"
-                    rows={6}
-                    placeholder="Contanos sobre tus productos o servicios"
+                    id="descripcion"
+                    name="descripcion"
+                    rows={5}
+                    value={formData.descripcion}
+                    onChange={handleChange}
                   ></textarea>
                 </div>
                 <button type="submit" className="btn-ver-mas">
-                  Enviar Solicitud
+                  Enviar Postulación
                 </button>
               </form>
             </div>
