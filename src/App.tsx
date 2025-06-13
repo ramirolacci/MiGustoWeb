ce0876527b14837673694e9bb298aa4d05ecf4b1
JoaquinTonizzo <joaquintonizzo1@gmail.com>
2025-06-13 12:11:38 -0300

    cambios en loadicon

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const AppContent: React.FC = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 1000);
 
     return () => {
       clearTimeout(timer);
