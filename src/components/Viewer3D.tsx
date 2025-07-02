b10c3f51607cb694f8a3155357fdb21d42a16b98
Ramiro <ramiroalejandrolacci19@gmail.com>
2025-07-02 15:24:06 -0300

    3D object modification in web version

--- src/components/Viewer3D.tsx
+++ src/components/Viewer3D.tsx
@@ -348,7 +348,7 @@ const Viewer3D: React.FC = () => {
         'min-camera-orbit': 'auto auto 3m',
         'max-camera-orbit': 'auto auto 3m',
         'interaction-prompt': 'none',
-        ...(isMobile ? { 'disable-pan': true } : {}),
+        'disable-pan': true,
         onLoad: () => setLoading(false),
       })}
     </div>
