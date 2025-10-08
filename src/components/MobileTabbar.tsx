ca3ae6e7f7c7c9f72e7f06c92091292c58607486
ramirolacci <ramiroalejandrolacci19@gmail.com>
2025-10-08 12:59:52 -0300

    Several fix in version mobile

--- src/components/MobileTabbar.tsx
+++ src/components/MobileTabbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import MobileSideMenu from './MobileSideMenu';
-import { getToken } from '../services/auth';
+// import { getToken } from '../services/auth';
 import './MobileTabbar.css';
 
 const MobileTabbar: React.FC = () => {
@@ -23,14 +23,29 @@ const MobileTabbar: React.FC = () => {
           <span>Inicio</span>
         </button>
         
+        {/**
+         * Botón original de Canje (comentado a pedido, sin eliminar código ni archivos)
+         *
+         * <button 
+         *   className="hm-tab" 
+         *   onClick={() => navigate('/canje')} 
+         *   aria-label="Canje" 
+         *   aria-current={location.pathname.startsWith('/canje') ? 'page' : undefined}
+         * >
+         *   <img src="/icons/tab-canje.svg" alt="Canje" />
+         *   <span>Canje</span>
+         * </button>
+         */}
+
+        {/* Botón reemplazo: Productos */}
         <button 
           className="hm-tab" 
-          onClick={() => navigate('/canje')} 
-          aria-label="Canje" 
-          aria-current={location.pathname.startsWith('/canje') ? 'page' : undefined}
+          onClick={() => navigate('/productos')} 
+          aria-label="Productos" 
+          aria-current={location.pathname.startsWith('/productos') ? 'page' : undefined}
         >
-          <img src="/icons/tab-canje.svg" alt="Canje" />
-          <span>Canje</span>
+          <img src="/icons/pizza.svg" alt="Productos" />
+          <span>Productos</span>
         </button>
 
         <button 
@@ -42,18 +57,33 @@ const MobileTabbar: React.FC = () => {
           <span>Pedir</span>
         </button>
 
+        {/**
+         * Botón original de Cuenta (comentado a pedido, sin eliminar código ni archivos)
+         *
+         * <button 
+         *   className="hm-tab" 
+         *   onClick={() => {
+         *     const token = getToken();
+         *     if (token) navigate('/mi-cuenta');
+         *     else navigate('/login');
+         *   }} 
+         *   aria-label="Cuenta" 
+         *   aria-current={(location.pathname.startsWith('/mi-cuenta') || location.pathname.startsWith('/perfil')) ? 'page' : undefined}
+         * >
+         *   <img src="/icons/tab-cuenta.svg" alt="Cuenta" />
+         *   <span>Cuenta</span>
+         * </button>
+         */}
+
+        {/* Botón reemplazo: Sucursales */}
         <button 
           className="hm-tab" 
-          onClick={() => {
-            const token = getToken();
-            if (token) navigate('/mi-cuenta');
-            else navigate('/login');
-          }} 
-          aria-label="Cuenta" 
-          aria-current={(location.pathname.startsWith('/mi-cuenta') || location.pathname.startsWith('/perfil')) ? 'page' : undefined}
+          onClick={() => navigate('/sucursales')} 
+          aria-label="Sucursales" 
+          aria-current={location.pathname.startsWith('/sucursales') ? 'page' : undefined}
         >
-          <img src="/icons/tab-cuenta.svg" alt="Cuenta" />
-          <span>Cuenta</span>
+          <img src="/icons/tab-sucursales.svg" alt="Sucursales" />
+          <span>Sucursales</span>
         </button>
         
         <button 
