be609a8275fd32df0a8f719ffc46f38d35fe9e92
ramirolacci <ramiroalejandrolacci19@gmail.com>
2025-10-15 13:12:11 -0300

    Updating images on pizza product cards

--- src/data/pizzasData.ts
+++ src/data/pizzasData.ts
@@ -2,6 +2,7 @@
 
 export interface Pizza {
     imagen: string;
+    imagenCard?: string;
     titulo: string;
     descripcion: string;
     esRecomendado?: boolean;
@@ -12,77 +13,91 @@ export interface Pizza {
 export const pizzas: Pizza[] = [
     {
         imagen: "https://i.postimg.cc/50Lkjt7H/Muzza-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/cH0k2TN8/Pizza-Muzzarella.jpg",
         titulo: "Muzzarella",
         descripcion: "Pizza grande de Muzzarella elaborada con salsa de tomate casera artesanal.",
         esRecomendado: true
     },
     {
         imagen: "https://i.postimg.cc/50Lkjt7H/Muzza-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/0NwdnSMR/Pizza-Doble-Muzzarella.png",
         titulo: "Doble muzzarella",
         descripcion: "Pizza grande de Muzzarella doble elaborada con salsa de tomate casera artesanal.",
         esRecomendado: true
     },
     {
         imagen: "https://i.postimg.cc/7hPMDPpr/Muzza-con-Jamon-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/htx1bxQH/Pizza-Muzzarella-con-Jamon.png",
         titulo: "Muzzarella con jamón",
         descripcion: "Pizza grande de Muzzarella elaborada con jamón cocido y salsa de tomate.",
         esRecomendado: true
     },
     {
         imagen: "https://i.postimg.cc/Y9rmLn81/Jamon-con-morrones-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/4yV6GHRp/Pizza-Jamon-con-Morrones.png",
         titulo: "Jamón con morrones",
         descripcion: "Pizza grande de Muzzarella elaborada con jamón cocido y morrones asados."
     },
     {
         imagen: "https://i.postimg.cc/gcv2bqDp/Napo-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/50vYQc7B/Pizza-Napolitana.png",
         titulo: "Napolitana",
         descripcion: "Pizza grande de queso Muzzarella elaborada con rodajas de tomate fresco y topping de salsa casera de provenzal.",
         esRecomendado: true
     },
     {
         imagen: "https://i.postimg.cc/pL9NRPHj/Napo-con-Jamon-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/ZRk0S7bg/Pizza-Napolitana-Con-Jamon.png",
         titulo: "Napolitana con jamón",
         descripcion: "Pizza grande de queso Muzzarella elaborada con jamón cocido, rodajas de tomate fresco y topping de salsa casera de provenzal."
     },
     {
         imagen: "https://i.postimg.cc/zfSx1QVW/Provolone-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/cHM1Nd5Z/Pizza-Provolone.png",
         titulo: "Provolone",
         descripcion: "Pizza grande de queso Muzzarella elaborada con queso provolone, salsa de tomate, con orégano y ají molido."
     },
     {
         imagen: "https://i.postimg.cc/RZvRLRPZ/Provolone-con-Jamon-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/c1FZ79pT/Pizza-Provolone-con-Jamon.png",
         titulo: "Provolone con jamón",
         descripcion: "Pizza grande de queso Muzzarella elaborada con queso provolone, jamón cocido, salsa de tomate, con orégano y ají molido."
     },
     {
         imagen: "https://i.postimg.cc/Kc9v8stP/Panceta-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/CxqzQpYL/Pizza-Muzzarella-con-Panceta.png",
         titulo: "Panceta",
         descripcion: "Pizza grande de Muzzarella elaborada con panceta."
     },
     {
         imagen: "https://i.postimg.cc/g0CLNcYH/Roquefort-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/Dytzsps7/Pizza-Roquefort.png",
         titulo: "Roquefort",
         descripcion: "Pizza grande de queso muzzarella y queso roquefort."
     },
     {
         imagen: "https://i.postimg.cc/HWqHnJ62/Roquefort-con-Jamon-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/wj863yKP/Pizza-Roquefort-con-Jamon.png",
         titulo: "Roquefort con jamón",
         descripcion: "Pizza grande de queso muzzarella, queso roquefort y jamón cocido."
     },
     {
         imagen: "https://i.postimg.cc/9MqmgyxD/Cuatro-Quesos-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/jS4LbQwq/Pizza-Cuatro-Quesos.png",
         titulo: "Cuatro quesos",
         descripcion: "Pizza 4 quesos mezcla de queso muzzarella elaborada con queso roquefort y provolone.",
         esVegetariano: true
     },
     {
         imagen: "https://i.postimg.cc/V6FG8VZp/Provenzal-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/63k9Ry2z/Pizza-Provenzal.png",
         titulo: "Provenzal",
         descripcion: "Pizza grande de queso Muzzarella elaborada con ajo deshidratado y perejil.",
         esVegetariano: true
     },
     {
         imagen: "https://i.postimg.cc/HLdbgkGW/Fugazzeta-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/x8V28hhw/Fugazzeta.png",
         titulo: "Fugazzeta",
         descripcion: "Pizza grande de queso Muzzarella elaborada con cebolla blanca y provolone.",
         esVegetariano: true,
@@ -90,48 +105,57 @@ export const pizzas: Pizza[] = [
     },
     {
         imagen: "https://i.postimg.cc/135Ft5Rv/Fugazzeta-con-Panceta-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/k4xH14Y5/Fugazzeta-con-Panceta.png",
         titulo: "Fugazzeta con panceta",
         descripcion: "Pizza grande de queso Muzzarella elaborada con cebolla blanca, panceta y provolone."
     },
     {
         imagen: "https://i.postimg.cc/WbqKyTcf/Fugazzeta-con-Jamon-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/3N8ttZvK/Fugazzeta-con-Jamon.png",
         titulo: "Fugazzeta con jamón",
         descripcion: "Pizza grande de queso Muzzarella elaborada con cebolla blanca, jamón cocido y provolone."
     },
     {
         imagen: "https://i.postimg.cc/MGwpT9Yq/Caprese-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/k54VnRPp/Pizza-Caprese.png",
         titulo: "Caprese",
         descripcion: "Pizza grande de queso Muzzarella elaborada con hojas de albahaca, tomate y aceite de oliva.",
         esVegetariano: true
     },
     {
         imagen: "https://i.postimg.cc/TYHkfSf5/Jamon-Tomate-y-Huevo-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/hPctDQ9S/Pizza-Jamon-Tomate-y-Huevo.png",
         titulo: "Jamón, tomate y huevo",
         descripcion: "Pizza grande de queso Muzzarella elaborada con tomate fresco, jamón y huevo cocidos."
     },
     {
         imagen: "https://i.postimg.cc/dVygXvrV/Muzza-con-Huevo-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/Dyxn7jfz/Pizza-Muzzarella-Con-Huevo.png",
         titulo: "Muzzarella con huevo",
         descripcion: "Pizza grande de queso Muzzarella elaborada con tomate fresco y huevo cocido.",
         esVegetariano: true
     },
     {
         imagen: "https://i.postimg.cc/hGhsw1Hb/Jamon-y-huevo-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/cHSVRw13/Pizza-Jamon-con-Huevo.png",
         titulo: "Jamón y huevo",
         descripcion: "Pizza grande de queso Muzzarella elaborada con jamón y huevo cocidos."
     },
     {
         imagen: "https://i.postimg.cc/3NMHWRgV/Jamon-tomate-huevo-y-roquefort-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/rsYnPrck/Pizza-Jamon-Tomate-Huevo-y-Roquefort.png",
         titulo: "Jamón, tomate, huevo y roquefort",
         descripcion: "Pizza grande de queso Muzzarella elaborada con queso roquefort, tomate fresco y huevo cocido."
     },
     {
         imagen: "https://i.postimg.cc/c4BryyW0/Provolone-Jamon-y-Morron-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/ZKBVq04z/Pizza-Provolone-Jamon-y-Morron.png",
         titulo: "Provolone, jamón y morrón",
         descripcion: "Pizza grande de queso Muzzarella elaborada con queso provolone, jamón cocido y morrones asados."
     },
     {
         imagen: "https://i.postimg.cc/R05SNg4X/Panceta-y-verdeo-Photoroom.png",
+        imagenCard: "https://i.postimg.cc/D0WdmYkT/Pizza-Panceta-con-Verdeo.png",
         titulo: "Panceta y verdeo",
         descripcion: "Pizza grande de queso Muzzarella elaborada con panceta y verdeo."
     }
